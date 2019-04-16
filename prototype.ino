d0a4a75bc83eee57283b11c345907b9dcf4ca735
Sirajuddin Asjad <sirasjad@gmail.com>
2019-04-16 17:49:18 +0200

    Update

--- prototype.ino
+++ prototype.ino
@@ -1,5 +1,7 @@
 #include <Servo.h>
 
+Servo esc;          // Creating a servo class withe name as ESC
+
 Servo servo1;
 Servo servo2;
 const int servoPin1 = 6;
@@ -10,30 +12,35 @@ volatile int buttonState = 0;
 volatile unsigned long lastInterrupt = 0;
 
 void setup() {
+  esc.attach(8);      // Specify the ESC signal pin, Here as D8
+  esc.writeMicroseconds(1000);  //initialize the signal to 1000
   servo1.attach(servoPin1);
   servo2.attach(servoPin2);
   pinMode(buttonPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(buttonPin), setGitterPosition, RISING);
-  Serial.begin(9600);
 }
 
 void loop() {
-  //
+  int val;      // Creating a variable val
+  val = analogRead(A0);   // Read input from analog pin a0 and store in val
+  val = map(val, 0, 1023, 1000, 2000);    // mapping the val to minimum and maximum input
+  esc.writeMicroseconds(val);   // using val as the signal to ESC
+  
 }
 
 void setGitterPosition(){
-  if((millis() - lastInterrupt) > 2000){
+  if((millis() - lastInterrupt) > 500){
     switch(buttonState){
       case 0:{
         Serial.println("Opening gate!");
-        for(int pos = 0; pos < 180; pos++){
+        for(int pos = 0; pos < 110; pos++){
           servo1.write(pos);
           servo2.write(pos);
         }
       } break;
       case 1:{
         Serial.println("Closing gate!");
-        for(int pos = 180; pos > 15; pos--) { 
+        for(int pos = 110; pos > 30; pos--) { 
           servo1.write(pos);
           servo2.write(pos);
         }
